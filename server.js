115381be1d2fa5eca47e017e640ad853dd943c3d
Asilazeez <asilazeez@gmail.com>
2016-09-29 08:28:38 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -4,6 +4,34 @@ var path = require('path');
 
 var app = express();
 app.use(morgan('combined'));
+var articleone={
+    title: "Article one created by Asil",
+    content:'<p> Hai I am Asil this is my HTML page, During my college days I have try to create some web app. So I got some ides about this course. Now Im working in nucore software solution as Quality Assuarance engineer. Now Im looking forward to evolve myself to a super tester who got a detail idea about development.</p>',
+    head: "ARTICLE ONE"
+};
+var template='<html>
+    <head>
+        <title>
+            ${title}
+        </title>
+        <meta name="viewport" content = "width-device-width,initial-scale=1"/>
+         <link href="/ui/style.css" rel ="stylesheet"/>
+    </head>
+    <body>
+        <div class = "container">
+            <div>
+                <a href="/">Home</a>
+            </div>
+            <div>
+                <h3>${head}</h3>
+            </div>
+            <div>
+                ${content}
+            </div>
+        </div>
+    </body>
+</html> '  ;
+
 
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
