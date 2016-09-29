0654cea945f2648accdce80d002d4f67b02f73a0
Asilazeez <asilazeez@gmail.com>
2016-09-29 06:59:04 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ app.get('/', function (req, res) {
 });
 
 app.get('/articleone', function (req, res) {
-   res.sendFile(path.join(_dirname, 'ui', 'article_one.html'));
+   res.sendFile(path.join(__dirname, 'ui', 'article_one.html'));
  // res.send("response: to Article one ");
 });
 app.get('/ui/style.css', function (req, res) {
